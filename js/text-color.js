d7e3c9203e585dadf61958ffcee30977aac92128
Min Jae Kim <kmj6832596@naver.com>
2023-01-16 19:03:20 +0900

    Add files via upload

---
+++ js/text-color.js
@@ -0,0 +1,4 @@
+var heading = document.querySelector("#heading"); /* heading이라는 웹 요소를 가져온다. */
+heading.onclick = function(){ /* 클릭을 했을 때 함수를 실행한다. */
+    heading.style.color = "red"; /* heading이라는 웹요소에 글자 색깔을 빨간색으로 바꾼다. */
+}
